ab8ffbc44d964bc782569908ee4916158dcb1500
gitnickolson <nicknickolson@web.de>
2023-08-11 13:53:16 +0200

    Updated the code

--- main.rb
+++ main.rb
@@ -1,103 +1,100 @@
 class Board
-  @@Board_size = 3
-  @@Row_size = 5
-
   def initialize
-    @board_array = Array.new(@@Board_size) { Array.new(@@Row_size) }
-    set_up
-    game_loop(@board_array)
+    @COLUMN_SIZE = 3
+    @ROW_SIZE = 3
+    @@board_visual = Array.new(@COLUMN_SIZE) {Array.new(@ROW_SIZE)}
   end
 
-  def set_up
-    @board_array.each_with_index do |row_array, row_index|
-      row_array.each_with_index do |slot, slot_index|
-        if slot_index.odd?
-          @board_array[row_index][slot_index] = "|"
-        elsif slot_index.even?
-          @board_array[row_index][slot_index] = " "
-        end
-      end
-    end
+  def update_board(index, symbol)
+    @@board_visual[index] = symbol
     print_board
   end
 
-  protected
-
-  def game_loop(board)
-    turn_counter = 0
-
-    if turn_counter.even?
-      puts "Player 1: Your turn! Please enter the number of the field you want to label (1-9)."
-      player_one_input = gets.chomp.to_i - 1
-
-      if player_one_input.between?(0, 8)
-        case player_one_input
-        when (0..2) && input_not_XO(0, player_one_input)
-          board[0][player_one_input] = "X"
-          print_board
-          turn_counter += 1
-
-        when (3..5) && input_not_XO(1, player_one_input)
-          board[1][player_one_input] = "X"
-          print_board
-          turn_counter += 1
-
-        when (6..8) && input_not_XO(2, player_one_input)
-          board[2][player_one_input] = "X"
-          print_board
-          turn_counter += 1
-
-        else
-          puts "\bPlayer 1: Please enter the number of a field that wasn't labeled yet."
-          game_loop(@board_array)
-        end
-      else
-        puts "Player 1: Please input a number between 1 and 9"
-        game_loop(@board_array)
-      end
-    elsif turn_counter.odd?
-      puts "Player 2: Your turn! Please enter the number of the field you want to label (1-9)."
-      player_two_input = gets.chomp.to_i - 1
-
-      if player_two_input.between?(0, 8)
-        case player_two_input
-        when (0..2) && input_not_XO(0, player_two_input)
-          board[0][player_two_input] = "O"
-          print_board
-          turn_counter += 1
-
-        when (3..5) && input_not_XO(1, player_two_input)
-          board[1][player_two_input] = "O"
-          print_board
-          turn_counter += 1
-
-        when (6..9) && input_not_XO(2, player_two_input)
-          board[2][player_two_input] = "O"
-          print_board
-          turn_counter += 1
-
-        else
-          puts "\bPlayer 2: Please enter the number of a field that wasn't labeled yet."
-          game_loop(@board_array)
-        end
-      else
-        puts "Player 2: Please input a number between 1 and 9"
-        game_loop(@board_array)
-      end
-    end
-  end
-
-  def input_not_XO(board_index, player_input)
-    @board_array[board_index][player_input] != "X" && @board_array[board_index][player_input] != "O"
+  def set_up
+    @@board_visual.map! { |a, b, c| a = "", b = "", c = ""}
   end
 
   def print_board
-    p @board_array[0].join(" ")
-    p @board_array[1].join(" ")
-    p @board_array[2].join(" ")
+    p @@board_visual[0].join(" | ")
+    p @@board_visual[1].join(" | ")
+    p @@board_visual[2].join(" | ")
+  end
+
+  def not_labeled?(board_index, player_input)
+    @@board_visual[board_index][player_input] != "X" && @@board_visual[board_index][player_input] != "O"
+  end
+end
+
+
+class Player < Board
+  attr_reader :symbol, :player_number
+
+  def initialize (player_number, symbol)
+    @player_number = player_number
+    @symbol = symbol
+  end
+
+  def get_player_input
+    @@player_input = gets.chomp.to_i
+  end
+end
+
+
+class Game < Player
+  attr_reader :player
+  @turn_counter
+
+  def initialize
+    @turn_counter = 0
+  end
+
+  def game_loop
+    player_input = @@player_input
+    if player_input.between?(0, 8)
+      if player_input.between?(0, 2) && not_labeled?(0, player_input)
+        p @symbol
+        update_board(player_input, @symbol)
+        @@turn_counter += 1
+
+      elsif player_input.between?(3, 5) && not_labeled?(1, player_input)
+        update_board(player_input, @symbol)
+        @@turn_counter += 1
+
+      elsif player_input.between?(6, 8) && not_labeled?(2, player_input)
+        update_board(player_input, @symbol)
+        @@turn_counter += 1
+
+      else
+        puts "Please enter a valid number for a field that isn't labeled yet."
+      end
+
+    else
+      puts "Please enter a number between 1 and 9"
+    end
+  end
+
+  def which_player?
+    if @turn_counter.even?
+      puts "Player 1: Your turn. Please enter a number between 1 and 9!"
+      player = 1
+    else
+      puts "Player 2: Your turn. Please enter a number between 1 and 9!"
+      player = 2
+    end
   end
 end
 
 #######################################################################################################################
 board = Board.new
+player_one = Player.new(1, "X")
+player_two = Player.new(2, "O")
+game = Game.new
+
+board.set_up
+board.print_board
+
+game.which_player?
+
+player_one.get_player_input
+playero_one.game
 
